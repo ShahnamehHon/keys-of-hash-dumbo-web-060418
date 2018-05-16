abadeb4e743f949998e9fbf96fe17fa34ad74a88
Ahmadali <ahmadali.azizov@outlook.com>
2018-05-16 21:50:55 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,5 @@
 class Hash
   def keys_of(*arguments)
-    self.collect {|k, e| k if args.include?(e)}.keep_if {|e| e}
+    self.collect {|k, e| k if arguments.include?(e)}.keep_if {|e| e}
   end
 end
